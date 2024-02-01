73badc0a04a31a546513be967f33aec0c5a2ecde
Dawid Szlachta <git@makingprogress.dev>
2024-02-01 16:13:16 +0100

    Moved defaultLimit const

--- query/lambda/handle_get_appearances.go
+++ query/lambda/handle_get_appearances.go
@@ -8,11 +8,13 @@ import (
 	"github.com/TrueBlocks/trueblocks-key/query/pkg/query"
 )
 
+const defaultAppearancesLimit = 100
+
 func handleGetAppearances(ctx context.Context, rpcRequest *query.RpcRequest) (response *query.RpcAppearancesResponse, err error) {
 	limit := rpcRequest.Parameters().PerPage
 	if limit == 0 {
 		// Just in case we forgot to define the limit in configuration
-		limit = defaultLimit
+		limit = defaultAppearancesLimit
 	}
 
 	if confLimit := cnf.Query.MaxLimit; confLimit > 0 {
