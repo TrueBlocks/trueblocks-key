8dc5297fd1625608b64ce57bf325aa7bc16cb02c
Dawid Szlachta <git@makingprogress.dev>
2024-03-04 12:45:01 +0100

    Optimized query

--- database/pkg/sql/addresses.go
+++ database/pkg/sql/addresses.go
@@ -8,12 +8,16 @@ import (
 
 func SelectAddressesInTx(appearancesTableName string, addressesTableName string) string {
 	return fmt.Sprintf(`
+WITH apps AS (
+    SELECT address_id
+    FROM %[1]s
+    WHERE block_number = $1 AND tx_id = $2
+    ORDER BY block_number DESC, tx_id ASC
+)
 SELECT addrs.address
-FROM %[1]s
-JOIN %[2]s addrs ON addrs.id = address_id
-WHERE block_number = $1 AND tx_id = $2
-LIMIT $3
-OFFSET $4;
+FROM apps
+JOIN %[2]s addrs ON addrs.id = apps.address_id
+ORDER BY address;
 `,
 		pgx.Identifier.Sanitize(pgx.Identifier{appearancesTableName}),
 		pgx.Identifier.Sanitize(pgx.Identifier{addressesTableName}),
